96ae149ef5645fcba3b736ccb8cd7532d3cfdf80
raj-sangani <rajsangani369@gmail.com>
2023-07-28 20:47:46 +0530

    Project Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [isBackgroundYellow, setIsBackgroundYellow] = useState(false);
+
+
+
+  const changeColor = () => {
+    setIsBackgroundYellow(!isBackgroundYellow);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={isBackgroundYellow? 'background-yellow':'background-green'} style={{
+      height: '100vh',
+      display: 'flex',
+    }}>
+      <button className="btn" onClick={changeColor}>Change Color</button>
     </div>
   );
 }
